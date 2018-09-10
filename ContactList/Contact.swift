f1a32e3e9316dcded5ab8e0b3191dc4539f84fea
Kendall Poindexter <kendall.poindexter@detroitlabs.com>
2018-09-10 00:23:01 -0400

    Added contact and contactlist swift files and a tableview

---
+++ ContactList/Contact.swift
@@ -0,0 +1,16 @@
+//
+//  Contact.swift
+//  ContactList
+//
+//  Created by Kendall Poindexter on 9/10/18.
+//  Copyright © 2018 Kendall Poindexter. All rights reserved.
+//
+
+import Foundation
+
+struct Contact {
+    
+    let firstName: String
+    let lastName: String
+    let phoneNumber: String
+}
---
+++ ContactList/ContactList.swift
@@ -0,0 +1,14 @@
+//
+//  ContactList.swift
+//  ContactList
+//
+//  Created by Kendall Poindexter on 9/10/18.
+//  Copyright © 2018 Kendall Poindexter. All rights reserved.
+//
+
+import Foundation
+
+struct ContactList {
+    let arrayOfContacts: [Contact] = [Contact(firstName: "Kendall", lastName: "Poindexter", phoneNumber: "313-377-4051")]
+}
+
